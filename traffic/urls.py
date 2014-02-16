ebfa29c6444f08df3b9c43fdce69eb3b1789c558
g2sacramento <ggsacramento2@addu.edu.ph>
2014-02-16 22:10:57 +0800

    Update

--- traffic/urls.py
+++ traffic/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import *
+from intersections.views import* 
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -8,10 +10,11 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 	
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^intersections/$', 'intersections.views.intersections'),
-	url(r'^login/$', 'intersections.views.login_user'),
-	url(r'^devices/$', 'intersections.views.devices'),
-	url(r'^roads/$', 'intersections.views.roads'),
-	url(r'^accidents/$', 'intersections.views.accidents'),
-	
+	#url(r'^intersections/$', 'intersections.views.intersections'),
+	url(r'^intersections/', intersections, name='intersections'),
+	url(r'^$', login_user, name='login'),
+	url(r'^devices/', devices, name='devices'),
+	url(r'^roads/', roads, name = 'roads'),
+	url(r'^accidents/', accidents, name='accidents'),
+	#url(r'^pages/', include('django.contrib.flatpages.urls')),
 )
